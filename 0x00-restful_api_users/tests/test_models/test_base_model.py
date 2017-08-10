b2c8a6fd67d9f40641b403c16c96cc85870ad4c3
madejean <madejean007@hotmail.fr>
2017-08-10 14:59:43 -0700

    running checkers

--- 0x00-restful_api_users/tests/test_models/test_base_model.py
+++ 0x00-restful_api_users/tests/test_models/test_base_model.py
@@ -7,16 +7,18 @@ from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
-
+    """testing intial values"""
     def test_initialization(self):
         self.assertIsNone(BaseModel.id)
         self.assertIsNone(BaseModel.created_at)
         self.assertIsNone(BaseModel.updated_at)
 
+    """testing for unique ids"""
     def test_unique_id(self):
         base_model = BaseModel()
         self.assertTrue(isinstance(base_model.id, uuid.UUID))
 
+    """testing created_at values"""
     def test_created_at(self):
         base_model = BaseModel()
         self.assertIsInstance(base_model.created_at, str)
@@ -25,6 +27,7 @@ class TestBaseModel(unittest.TestCase):
             datetime
         )
 
+    """testing updated_at values"""
     def test_updated_at(self):
         base_model = BaseModel()
         self.assertIsInstance(base_model.updated_at, str)
