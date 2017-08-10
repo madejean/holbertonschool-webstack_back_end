5f5b54fd9277cd3726e51f1fbd1ee1f18436c1f3
madejean <madejean007@hotmail.fr>
2017-08-10 14:56:59 -0700

    running checkers

--- 0x00-restful_api_users/tests/test_models/test_base_model.py
+++ 0x00-restful_api_users/tests/test_models/test_base_model.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""Unittest for BaseModel"""
 import unittest
 from datetime import datetime
 import uuid
 from models.base_model import BaseModel
+
 
 class TestBaseModel(unittest.TestCase):
 
@@ -18,12 +20,18 @@ class TestBaseModel(unittest.TestCase):
     def test_created_at(self):
         base_model = BaseModel()
         self.assertIsInstance(base_model.created_at, str)
-        self.assertIsInstance(datetime.strptime(base_model.created_at, "%Y-%m-%d %H:%M:%S.%f"), datetime)
+        self.assertIsInstance(
+            datetime.strptime(base_model.created_at, "%Y-%m-%d %H:%M:%S.%f"),
+            datetime
+        )
 
     def test_updated_at(self):
         base_model = BaseModel()
         self.assertIsInstance(base_model.updated_at, str)
-        self.assertIsInstance(datetime.strptime(base_model.updated_at, "%Y-%m-%d %H:%M:%S.%f"), datetime)
+        self.assertIsInstance(
+            datetime.strptime(base_model.updated_at, "%Y-%m-%d %H:%M:%S.%f"),
+            datetime
+        )
 
 if __name__ == '__main__':
     unittest.main()
