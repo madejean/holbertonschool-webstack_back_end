3d640cc68ff53b88772c4f7ee39b2ae7f05ac43f
madejean <madejean007@hotmail.fr>
2017-08-10 21:53:08 -0700

    running checkers

--- 0x00-restful_api_users/tests/test_models/test_user.py
+++ 0x00-restful_api_users/tests/test_models/test_user.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
+"""Unittest for User model"""
 import unittest
 import hashlib
 from models.base_model import BaseModel
 from models.user import User
 
 class TestUser(unittest.TestCase):
+    """testing displays fullname of user"""
     def test_name_display(self):
         user = User()
 
+    """testing displays reformatted user info"""
     def test__str__(self):
         user = User()
 
+    """testing password value"""
     def test_password(self):
         user = User()
-
-if __name__ == '__main__':
-    unittest.main()
