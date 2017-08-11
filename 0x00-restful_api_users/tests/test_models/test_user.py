0e9f7b44961cafc5c279fe1d81e42ef5a00f7dd2
madejean <madejean91@gmail.com>
2017-08-11 16:45:10 +0000

    added tests for display_name

--- 0x00-restful_api_users/tests/test_models/test_user.py
+++ 0x00-restful_api_users/tests/test_models/test_user.py
@@ -5,10 +5,27 @@ import hashlib
 from models.base_model import BaseModel
 from models.user import User
 
+
 class TestUser(unittest.TestCase):
+    def setUp(self):
+        self.user = User()
+
     """testing displays fullname of user"""
-    def test_name_display(self):
-        user = User()
+    def test_no_name_display(self):
+        self.assertIs(self.user.display_name(), "")
+
+    def test_email_display(self):
+        self.user.email = "hbtn@holbertonschool.com"
+        self.assertIs(self.user.display_name(), "hbtn@holbertonschool.com")
+
+    def test_firstname_display(self):
+        self.user.first_name = "Bob"
+        self.assertIs(self.user.display_name(), "Bob")
+
+    def test_fullname_display(self):
+        self.user.first_name = "Bob"
+        self.user.last_name = "Dylan"
+        self.assertEqual(self.user.display_name(), "Bob Dylan")
 
     """testing displays reformatted user info"""
     def test__str__(self):
