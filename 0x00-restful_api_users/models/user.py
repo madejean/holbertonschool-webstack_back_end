af7a2f9ed065a2764c3169abdcf740f87b7c45b1
madejean <madejean007@hotmail.fr>
2017-08-10 21:36:59 -0700

    running checkers

--- 0x00-restful_api_users/models/user.py
+++ 0x00-restful_api_users/models/user.py
@@ -32,11 +32,9 @@ class User(BaseModel, Base):
 
     """displays the full name of an User instance"""
     def display_name(self):
-        if self.email is None:
-            return ""
-        if self.first_name is None:
-            return ""
-        if self.last_name is None:
+        if self.email is None
+        and self.first_name is None
+        and self.first_name is None:
             return ""
         if self.first_name is None and self.last_name is None:
             return self.email
