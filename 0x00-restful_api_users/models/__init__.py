36dd13756bcba82fa886670d45ac790e84bd6cad
madejean <madejean007@hotmail.fr>
2017-08-12 11:09:49 -0700

    added comments

--- 0x00-restful_api_users/models/__init__.py
+++ 0x00-restful_api_users/models/__init__.py
@@ -4,11 +4,13 @@ import os
 from models.base_model import Base
 from models.user import User
 
+"""getting environment variables"""
 host = os.environ.get('HBNB_YELP_MYSQL_HOST')
 user = os.environ.get('HBNB_YELP_MYSQL_USER')
 password = os.environ.get('HBNB_YELP_MYSQL_PWD')
 db = os.environ.get('HBNB_YELP_MYSQL_DB')
 
+"""instance of a SQLAlchemy Engine"""
 db_engine = create_engine(
     'mysql+mysqldb://{}:{}@{}/{}'.format(user, password, host, db)
     )
@@ -17,6 +19,7 @@ if os.environ.get('HBNB_MYSQL_ENV') == 'test':
     User.metadata.drop_all()
 
 User.metadata.create_all(db_engine)
+"""instance of SQLAlchemy Session"""
 db_session = scoped_session(
     sessionmaker(bind=db_engine, expire_on_commit=False)
     )
