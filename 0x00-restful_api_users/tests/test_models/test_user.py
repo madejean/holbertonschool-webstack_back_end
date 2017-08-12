3e70eb9bb3de3beffcf612c8eb76e7989bce3f7d
madejean <madejean007@hotmail.fr>
2017-08-12 13:53:15 -0700

    running checkers

--- 0x00-restful_api_users/tests/test_models/test_user.py
+++ 0x00-restful_api_users/tests/test_models/test_user.py
@@ -22,15 +22,14 @@ class TestUser(unittest.TestCase):
         self.dict_user.last_name = "Dylan"
         self.d_user = self.dict_user.to_dict()
 
+# Testing display_name()
     def test_no_value_display(self):
-
         """
         testing displays empty if no user
         """
         self.assertIs(self.user.display_name(), "")
 
     def test_email_display(self):
-
         """
         testing email value display
         """
@@ -38,7 +37,6 @@ class TestUser(unittest.TestCase):
         self.assertIs(self.user.display_name(), "hbtn@holbertonschool.com")
 
     def test_firstname_display(self):
-
         """
         testing first name value display
         """
@@ -46,7 +44,6 @@ class TestUser(unittest.TestCase):
         self.assertIs(self.user.display_name(), "Bob")
 
     def test_lastname_display(self):
-
         """
         testing last name value display
         """
@@ -55,7 +52,6 @@ class TestUser(unittest.TestCase):
         self.assertIs(self.user.display_name(), "Dylan")
 
     def test_fullname_display(self):
-
         """
         testing full name value display
         """
@@ -64,7 +60,6 @@ class TestUser(unittest.TestCase):
         self.assertEqual(self.user.display_name(), "Bob Dylan")
 
     def test__str__(self):
-
         """
         testing reformatted user info display
         """
@@ -78,14 +73,12 @@ class TestUser(unittest.TestCase):
         )
 
     def test_no_password(self):
-
         """
         testing empty password
         """
         self.assertIsNone(self.user.password)
 
     def test_password(self):
-
         """
         testing password value
         """
@@ -96,14 +89,12 @@ class TestUser(unittest.TestCase):
         )
 
     def test_is_valid_password_none(self):
-
         """
         testing password None
         """
         self.assertFalse(self.user.is_valid_password(None))
 
     def test_password_is_not_valid(self):
-
         """
         testing false password
         """
@@ -111,7 +102,6 @@ class TestUser(unittest.TestCase):
         self.assertFalse(self.user.is_valid_password("tutu1234"))
 
     def test_password_is_valid(self):
-
         """
         testing valid password
         """
@@ -120,14 +110,12 @@ class TestUser(unittest.TestCase):
         self.assertTrue(self.user.is_valid_password("toto1234"))
 
     def test_id_to_dict_id(self):
-
         """
         testing id in dict
         """
         self.assertIsInstance(self.d_user["id"], str)
 
     def test_to_dict_updated_at(self):
-
         """
         testing updated_at in dict
         """
@@ -137,7 +125,6 @@ class TestUser(unittest.TestCase):
         )
 
     def test_to_dict_firstname(self):
-
         """
         testing first_name in dict
         """
@@ -151,7 +138,6 @@ class TestUser(unittest.TestCase):
         )
 
     def test_to_dict_email(self):
-
         """
         testing email in dict
         """
@@ -165,7 +151,6 @@ class TestUser(unittest.TestCase):
         )
 
     def test_to_dict_lastname(self):
-
         """
         testing last_name in dict
         """
@@ -179,7 +164,6 @@ class TestUser(unittest.TestCase):
         )
 
     def test_to_dict_created_at(self):
-
         """
         testing created_at in dict
         """
