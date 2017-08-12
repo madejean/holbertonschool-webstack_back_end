ab9c612b15b693d5664443f927fa2337d6bbfc91
madejean <madejean007@hotmail.fr>
2017-08-11 17:55:15 -0700

    added pep8 style

--- 0x00-restful_api_users/models/__init__.py
+++ 0x00-restful_api_users/models/__init__.py
@@ -5,14 +5,18 @@ from models.base_model import Base
 from models.user import User
 
 host = os.environ.get('HBNB_YELP_MYSQL_HOST')
-user =  os.environ.get('HBNB_YELP_MYSQL_USER')
+user = os.environ.get('HBNB_YELP_MYSQL_USER')
 password = os.environ.get('HBNB_YELP_MYSQL_PWD')
 db = os.environ.get('HBNB_YELP_MYSQL_DB')
 
-db_engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(user, password, host, db))
+db_engine = create_engine(
+    'mysql+mysqldb://{}:{}@{}/{}'.format(user, password, host, db)
+    )
 
 if os.environ.get('HBNB_MYSQL_ENV') == 'test':
     User.metadata.drop_all()
 
 User.metadata.create_all(db_engine)
-db_session = scoped_session(sessionmaker(bind=db_engine, expire_on_commit=False))
+db_session = scoped_session(
+    sessionmaker(bind=db_engine, expire_on_commit=False)
+    )
