e0f8e69e7989d5f6cb5a3ac822b8be9652abd0f3
madejean <madejean007@hotmail.fr>
2017-08-13 21:58:03 +0000

    added classmethod all()

--- 0x00-restful_api_users/models/base_model.py
+++ 0x00-restful_api_users/models/base_model.py
@@ -9,6 +9,38 @@ from sqlalchemy import Column, String, DateTime
 
 Base = declarative_base()
 
+class BaseModel():
+    """
+        Creating BaseModel class/ parent of all future models
+    """
+    id = Column(String(60), unique=True, nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(
+        DateTime,
+        nullable=False,
+        default=datetime.utcnow(),
+        onupdate=datetime.utcnow()
+    )
+
+    def __init__(self):
+        """
+            override the instance creation method
+        """
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+#!/usr/bin/python3
+"""
+    BaseModel
+"""
+import uuid
+from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime
+
+Base = declarative_base()
+
 
 class BaseModel():
     """
@@ -30,3 +62,9 @@ class BaseModel():
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+    @classmethod
+    def all(cls):
+        from models import db_session
+        return db_session.query(cls).order_by(cls.created_at).all()
+      
