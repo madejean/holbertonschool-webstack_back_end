0a3a784f2509362946d71cc35d43a9fb24b08240
madejean <madejean007@hotmail.fr>
2017-08-23 00:56:23 +0000

    fixed task 7

--- 0x01-restful_api_users/api/v1/views/index.py
+++ 0x01-restful_api_users/api/v1/views/index.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask import jsonify
 from api.v1.views import app_views
-from models.base_model import count
+from models.user import User
 
 @app_views.route('/status', strict_slashes=False)
 def status():
@@ -9,4 +9,4 @@ def status():
 
 @app_views.route('/stats', strict_slashes=False)
 def stats():
-    return jsonify(users=count)
+    return jsonify(users=User.count())
