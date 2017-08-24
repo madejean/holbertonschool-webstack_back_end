ff17e68fdf81750b119fa09fa3c47ddcfe7fe9a8
madejean <madejean91@gmail.com>
2017-08-24 23:06:29 +0000

    added task 9

--- 0x01-restful_api_users/api/v1/views/users.py
+++ 0x01-restful_api_users/api/v1/views/users.py
@@ -1,7 +1,8 @@
 from flask import Flask
-from flask import jsonify
+from flask import jsonify, abort
 from api.v1.views import app_views
 from models.user import User
+from models import db_session
 
 @app_views.route('/users', strict_slashes=False)
 def users():
@@ -10,3 +11,12 @@ def users():
     for user in users:
         user_array.append(user.to_dict())
     return jsonify(user_array)
+
+@app_views.route('/users/<user_id>', strict_slashes=False)
+def user(user_id):
+    user = db_session.query(User).get(user_id)
+    if user is None:
+        return abort(404)
+    else:
+        user.to_dict()
+        return jsonify(user)
