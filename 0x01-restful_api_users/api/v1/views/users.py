ea66195fb39fa57542bd9112a01262c66c0066ae
madejean <madejean91@gmail.com>
2017-08-25 00:57:38 +0000

    part of task 11

--- 0x01-restful_api_users/api/v1/views/users.py
+++ 0x01-restful_api_users/api/v1/views/users.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import jsonify, abort
+from flask import jsonify, abort, request
 from api.v1.views import app_views
 from models.user import User
 from models import db_session
@@ -30,3 +30,30 @@ def delete_user(user_id):
         db_session.delete(user)
         db_session.commit()
         return jsonify()
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def create_user():
+    if request.get_json():
+        json = request.get_json()
+        email = json.get('email')
+        password = json.get('password')
+        if email is None:
+            return jsonify(error="email missing"), 400
+        if password is None:
+            return jsonify(error="password missing"), 400
+        if json.get('first_name') is None and json.get('last_name') is None:
+            newUser = User()
+            newUser.email = json['email']
+            newUser.password = json['password']
+        else:
+            newUser = User()
+            newUser.email = json['email']
+            newUser.password = json['password']
+            newUser.first_name = json['first_name']
+            newUser.last_name = json['last_name']
+        db_session.add(newUser)
+        db_session.commit()
+        created_user = User.last().to_dict()
+        return jsonify(created_user), 201
+    else:
+        return jsonify(error="Wrong format")
