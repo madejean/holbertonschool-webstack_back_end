0533d6ef76a888a5734e8a34a6593501fbbb996d
madejean <madejean007@hotmail.fr>
2017-09-08 11:22:00 -0700

    running checkers

--- 0x01-restful_api_users/0-warmup_flask.py
+++ 0x01-restful_api_users/0-warmup_flask.py
@@ -4,7 +4,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+#defines index route
 @app.route('/', methods=['GET'], strict_slashes=False)
 def holberton_School():
     """
@@ -12,6 +12,6 @@ def holberton_School():
     """
     return 'Holberton School'
 
-
+#not executed when imported
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5000)
