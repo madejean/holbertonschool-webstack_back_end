ccec9950a74e15b653636f988560189ef870f69a
madejean <madejean007@hotmail.fr>
2017-09-08 11:11:50 -0700

    running checkers

--- 0x01-restful_api_users/1-warmup_flask.py
+++ 0x01-restful_api_users/1-warmup_flask.py
@@ -4,11 +4,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
+"""
+route /c
+"""
 @app.route('/c', methods=['GET'], strict_slashes=False)
 def C_is_fun():
     """
-    route /c returns the string C is fun!
+    returns the string C is fun!
     """
     return 'C is fun!'
 
