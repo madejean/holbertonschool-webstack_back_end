ba0fc394923875b503dca5ab353c2322724a8026
madejean <madejean007@hotmail.fr>
2017-09-08 11:25:38 -0700

    running checkers

--- 0x01-restful_api_users/0-warmup_flask.py
+++ 0x01-restful_api_users/0-warmup_flask.py
@@ -4,14 +4,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-#defines index route
-@app.route('/', methods=['GET'], strict_slashes=False)
-def holberton_School():
-    """
-    index route returns the string Holberton School
-    """
-    return 'Holberton School'
 
-#not executed when imported
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+@app.route('/', methods=['GET'])
+def run_flask():
+    app.url_map.strict_slashes = False
+    return "Holberton School"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
