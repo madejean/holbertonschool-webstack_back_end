9248ec0dbc9d8f0f387d06413c085cb3b300fc03
madejean <madejean007@hotmail.fr>
2017-09-08 14:10:03 -0700

    running checkers

--- 0x01-restful_api_users/3-warmup_flask.py
+++ 0x01-restful_api_users/3-warmup_flask.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+endpoint returns a json structure
+"""
 from flask import Flask
 from flask import jsonify
 import os
@@ -11,21 +14,25 @@ HBNB_API_HOST = os.environ.get('HBNB_API_HOST')
 
 @app.route('/', strict_slashes=False)
 def holberton_School():
+    """
+    returns the string Holberton School
+    """
     return 'Holberton School'
 
 
 @app.route('/c', strict_slashes=False)
 def C_is_fun():
+    """
+    returns the string C is fun!
+    """
     return 'C is fun!'
-
-
-"""
-returns a json structure
-"""
 
 
 @app.route('/hbtn', strict_slashes=False)
 def hbtn():
+    """
+    returns a json structure
+    """
     return jsonify(C="is fun", Python="is cool", Sysadmin="is hiring")
 
 if __name__ == '__main__':
