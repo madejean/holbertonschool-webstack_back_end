b88edc1dfa6512958299c74a88a4138af01fd839
madejean <madejean007@hotmail.fr>
2017-09-08 14:08:01 -0700

    running checkers

--- 0x01-restful_api_users/2-warmup_flask.py
+++ 0x01-restful_api_users/2-warmup_flask.py
@@ -1,35 +1,32 @@
 #!/usr/bin/python3
+"""
+defining some routes
+"""
 from flask import Flask
 import os
 
 app = Flask(__name__)
 
-"""
-retrieving port and host from environment variables
-"""
-
-
+"""retrieving port and host from environment variables"""
 HBNB_API_PORT = os.environ.get('HBNB_API_PORT')
 HBNB_API_HOST = os.environ.get('HBNB_API_HOST')
-
-"""
-defining some routes
-"""
 
 
 @app.route('/', strict_slashes=False)
 def holberton_School():
+    """
+    returns the string Holberton School
+    """
     return 'Holberton School'
 
 
 @app.route('/c', strict_slashes=False)
 def C_is_fun():
+    """
+    returns the string C is fun!
+    """
     return 'C is fun!'
 
-"""
-run the app through host and port
-"""
-
-
+"""run the app through host and port"""
 if __name__ == '__main__':
     app.run(port=HBNB_API_PORT, host=HBNB_API_HOST)
