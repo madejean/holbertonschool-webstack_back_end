83c53734893985140f1b929a87e471e2dceb7fe0
madejean <madejean007@hotmail.fr>
2017-10-01 00:12:57 +0000

    added base64 part

--- 0x02-restful_api_users/api/v1/auth/basic_auth.py
+++ 0x02-restful_api_users/api/v1/auth/basic_auth.py
@@ -4,6 +4,28 @@ creating Basic authentication
 """
 from flask import Flask
 from api.v1.auth.auth import Auth
+import base64
 
 class BasicAuth(Auth):
-    pass
+    def extract_base64_authorization_header(self, authorization_header):
+        if authorization_header is None or type(authorization_header) != str or not "Basic" in authorization_header:
+            return None
+        else:
+            return authorization_header.split()[-1]
+
+    def decode_base64_authorization_header(self, base64_authorization_header):
+        if base64_authorization_header is None or type(base64_authorization_header) != str:
+            return None
+        try:
+            if base64.b64decode(base64_authorization_header):
+                return base64.b64decode(base64_authorization_header).decode('utf-8')
+        except:
+            return None
+
+    def extract_user_credentials(self, decoded_base64_authorization_header):
+        if decoded_base64_authorization_header is None or type(decoded_base64_authorization_header) != str or not ":" in decoded_base64_authorization_header:
+            return None, None
+        else:
+            email = decoded_base64_authorization_header.split(':')[0]
+            password = decoded_base64_authorization_header.split(':')[-1]
+            return email, password
