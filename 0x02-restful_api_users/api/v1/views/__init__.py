d119e5e7028f2e561935b5203684266e8466856d
madejean <madejean007@hotmail.fr>
2017-10-02 23:29:16 +0000

    running checkers

--- 0x02-restful_api_users/api/v1/views/__init__.py
+++ 0x02-restful_api_users/api/v1/views/__init__.py
@@ -3,11 +3,11 @@
 creating a Blueprint instance
 """
 from flask import Blueprint
-from api.v1.views.index import *
-from api.v1.views.users import *
-
 
 """
 creating blueprint instance
 """
 app_views = Blueprint('app_views', __name__, url_prefix="/api/v1")
+
+from api.v1.views.index import *
+from api.v1.views.users import *
