fca10584efc621ca6caa7b3ae0b0e0008adaa534
madejean <madejean91@gmail.com>
2017-11-01 20:49:03 +0000

    running checkers

--- 0x03-restful_api_users/api/v1/app.py
+++ 0x03-restful_api_users/api/v1/app.py
@@ -75,7 +75,8 @@ def before_request():
                 '/api/v1/auth_session/login/']
             ) is False:
         return
-    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+    if (auth.authorization_header(request) is None and
+            auth.session_cookie(request) is None):
         return abort(401)
     if auth.current_user(request) is None:
         return abort(403)
