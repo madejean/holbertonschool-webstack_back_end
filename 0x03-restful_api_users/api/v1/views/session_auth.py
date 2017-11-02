c7e699d2092fc2211df240e8022aac2268155154
madejean <madejean91@gmail.com>
2017-11-02 17:31:42 +0000

    running checkers

--- 0x03-restful_api_users/api/v1/views/session_auth.py
+++ 0x03-restful_api_users/api/v1/views/session_auth.py
@@ -11,30 +11,6 @@ from models.user import User
 from models import db_session
 
 
-@app_views.route('/auth_session/login', methods=['POST'], strict_slashes=False)
-def login():
-    """
-    login route
-    """
-    email = request.form.get('email')
-    password = request.form.get('password')
-    if email is None:
-        return jsonify(error="email missing"), 400
-    if password is None:
-        return jsonify(error="password missing"), 400
-    user = db_session.query(User).filter_by(email=email).first()
-    if user is None:
-        return jsonify(error="no user found for this email"), 404
-    if user.is_valid_password(password) is False:
-        return jsonify(error="wrong password"), 401
-    sessionID = auth.create_session(user.id)
-    d_user = user.to_dict()
-    HBNB_YELP_SESSION_NAME = os.environ.get('HBNB_YELP_SESSION_NAME')
-    response = jsonify(state=0, msg='success')
-    response.set_cookie(HBNB_YELP_SESSION_NAME, sessionID)
-    return jsonify(d_user)
-
-
 @app_views.route('/auth_session/logout',
                  methods=['DELETE'], strict_slashes=False)
 def logout():
