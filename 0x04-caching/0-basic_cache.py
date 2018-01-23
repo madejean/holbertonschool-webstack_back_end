ca641468a61d5750ce3b7309d043452a49ae6a15
madejean <madejean007@hotmail.fr>
2018-01-23 14:03:46 -0800

    adding comments

--- 0x04-caching/0-basic_cache.py
+++ 0x04-caching/0-basic_cache.py
@@ -8,11 +8,13 @@ class BasicCache(BaseCaching):
         - caching system
     """
     def put(self, key, item):
+        """ assign key value to cache_data dictionary """
         if not key or not item:
             pass
         self.cache_data[key] = item
 
     def get(self, key):
+        """ retrieves the value linked to the key """
         if key not in self.cache_data:
             return None
         return self.cache_data[key]
