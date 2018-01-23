771498e8bee42b28985576b1d4796b371f1e9210
madejean <madejean007@hotmail.fr>
2018-01-23 14:27:53 -0800

    adding comments

--- 0x04-caching/2-lifo_cache.py
+++ 0x04-caching/2-lifo_cache.py
@@ -7,11 +7,20 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LIFOCache(BaseCaching):
+    """ LIFOCache defines:
+        - caching system with last in last out algorithm
+    """
+
     def __init__(self):
+        """ defining overloading method """
         super().__init__()
         self.queue = list()
 
     def put(self, key, item):
+        """ assigns key value to cache_data dictionary
+            if cache_data dictionary exceeds its limit
+            the last item gets removed
+        """
         if key is None or item is None:
             pass
         if(len(self.cache_data) >= self.MAX_ITEMS and key not in self.queue):
@@ -22,6 +31,7 @@ class LIFOCache(BaseCaching):
         self.cache_data[key] = item
 
     def get(self, key):
+        """ retrieves the value linked to the key """
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
