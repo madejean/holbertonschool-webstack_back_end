cf8d05dfdd9a562044351b7422bd99073358f25e
madejean <madejean007@hotmail.fr>
2018-01-24 15:12:49 -0800

    running on python 3.4

--- 0x04-caching/1-fifo_cache.py
+++ 0x04-caching/1-fifo_cache.py
@@ -12,19 +12,22 @@ class FIFOCache(BaseCaching):
     def __init__(self):
         """ defining overloading method """
         super().__init__()
+        self.queue = []
 
     def put(self, key, item):
         """ assigns key value to cache_data dictionary
             if cache_data dictionary exceeds its limit
             the first item gets removed
         """
-        if not key or not item:
+        if key is None or item is None:
             return
+        self.queue.append(key)
         self.cache_data[key] = item
-        if len(self.cache_data) > self.MAX_ITEMS:
-            first_item = list(self.cache_data)[0]
+        if(len(self.cache_data) > self.MAX_ITEMS):
+            first_item = self.queue[0]
             self.cache_data.pop(first_item)
-            print("DISCARD: {}".format(first_item))
+            self.queue.pop(0)
+            print("DISCARD", first_item)
 
     def get(self, key):
         """ retrieves the value linked to the key """
