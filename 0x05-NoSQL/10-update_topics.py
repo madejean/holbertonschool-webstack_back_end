d822dd493adb132c11ad0b38d65d13c3368a0c42
madejean <madejean007@hotmail.fr>
2018-03-09 21:55:58 +0000

    adding comments

--- 0x05-NoSQL/10-update_topics.py
+++ 0x05-NoSQL/10-update_topics.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+""" changes all topics of a school document based on the name """
+
+
 def update_topics(mongo_collection, name, topics):
+    """updates a collection by name"""
     mongo_collection.update(
-        { "name": name },
-        { "$set": { "topics": topics }}
+        {"name": name},
+        {"$set": {"topics": topics}}
     )
