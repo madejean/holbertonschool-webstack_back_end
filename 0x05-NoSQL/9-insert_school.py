0ecaa327f1b10a6d4f7d8dbea7eb271d272594c3
madejean <madejean007@hotmail.fr>
2018-03-09 21:53:26 +0000

    running checkers

--- 0x05-NoSQL/9-insert_school.py
+++ 0x05-NoSQL/9-insert_school.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+""" inserts a new document in a collection """
+
+
 def insert_school(mongo_collection, **kwargs):
+    """returns newly inserted school id"""
     result = mongo_collection.insert_one(kwargs)
     return result.inserted_id
